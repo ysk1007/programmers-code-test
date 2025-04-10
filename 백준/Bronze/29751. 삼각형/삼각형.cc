b87829ff2b2070e9c121be81e2aab06ed97cca16
okonys <62321546+ysk1007@users.noreply.github.com>
2025-04-10 12:45:10 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+// 삼각형 #29751
+int main(){
+    double w,h;
+    cout<<fixed;
+    cout.precision(1);
+    cin >> w >> h;
+    cout << (float)(w * h / 2.0);
+}
