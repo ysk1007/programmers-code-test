619528f2ba5a747e5abf8a7d1ad77f4d53c5d3ac
okonys <62321546+ysk1007@users.noreply.github.com>
2025-04-15 12:18:58 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,18 +5,18 @@ using namespace std;
 // Hashing #15829
 int main(){
     int n;
-    long long result = 0;
+    long long sum = 0;
+    long long r = 1;
+    long long m = 1234567891;
     string s;
     cin >> n >> s;
 
-    int i = 0;
-    for(char c : s){
-        int sum = ((c - 'a') + 1) * pow(31,i);
-        result += sum;
-        i++;
+    for(int i = 0 ; i < n ; i++){
+        sum += (long(s[i] - 'a' + 1) * r) % m;
+        r = (r * 31) % m;
     }
 
-    cout << result;
+    cout << sum % m;
 
     return 0;
 }
