d58fb69eebd9fd88969f8a1a52399085f1a6542f
okonys <62321546+ysk1007@users.noreply.github.com>
2025-04-16 14:22:59 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+// Site Score #20254
+int main(){
+    int ur,tr,uo,to;
+    cin >> ur >> tr >> uo >> to;
+    cout << ur*56 + tr*24 + uo*14 + to*6;
+
+    return 0;
+}
