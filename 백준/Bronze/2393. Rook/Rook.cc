f3f6dc6a0d1ee637108a65973eb576980e537d25
okonys <62321546+ysk1007@users.noreply.github.com>
2025-04-16 14:18:37 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+// Rook #2393
+int main(){
+    cout << "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n";
+    cout << "     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n";
+    cout << "  __/       \\__\n /             \\\n/_______________\\";
+}
