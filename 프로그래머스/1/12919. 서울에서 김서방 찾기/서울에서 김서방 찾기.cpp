721b076932916b5035d51bcb347f5094d7978e31
okonys <62321546+ysk1007@users.noreply.github.com>
2025-05-02 12:52:20 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<string> seoul) {
+    return "김서방은 "+to_string(find(seoul.begin(),seoul.end(),"Kim") - seoul.begin())+"에 있다";
+}
