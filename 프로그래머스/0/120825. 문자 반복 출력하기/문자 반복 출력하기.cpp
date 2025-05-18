be3aac894d857b567d6a1fda849f51471688b644
okonys <62321546+ysk1007@users.noreply.github.com>
2025-05-18 18:54:33 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    
+    for(int i = 0 ; i < my_string.length() ; i++){
+        answer.append(n,my_string[i]);
+        //answer += string(n,my_string[i]);
+    }
+    
+    return answer;
+}
