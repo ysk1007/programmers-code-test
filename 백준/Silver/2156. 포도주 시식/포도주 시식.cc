52fa2fcd2dc5a164dd2fb29535a4e414064bd2db
okonys <62321546+ysk1007@users.noreply.github.com>
2025-09-30 12:42:49 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,41 +1,39 @@
-#include<iostream>
+    #include<iostream>
+    #include<vector>
+    using namespace std;
 
-using namespace std;
+    // 포도주 시식 #2156
+    int main(){
+        int t, result = 0;
+        cin >> t;
+        vector<int> vc(t+1);            // 포도주
+        vector<int> dp(t+1,0);          // dp
 
-// 포도주 시식 #2156
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+        for(int i = 1 ; i <= t ; i++){  // 포도주 초기화
+            cin >> vc[i];
+        }
 
-    int t;
-    int dp[100001] = {0};
+        if(t==1){                       // 포도주가 1잔 밖에 없음
+            cout << vc[1];
+            return 0;
+        }
 
-    cin >> t;
-    int al[t] = {0};
+        if(t==2){                       // 포도주가 2잔 밖에 없음
+            cout << vc[1] + vc[2];
+            return 0;
+        }
 
-    for(int i = 0 ; i < t ; i++){
-        cin >> al[i];
+        dp[1] = vc[1];                                      // dp[1]
+        dp[2] = vc[2] + vc[1];                              // dp[2]
+        dp[3] = max(dp[2],max(vc[3]+vc[1],vc[3]+vc[2]));    // dp[3]
+
+        result = dp[3];                                     // 현재까지 최대 양은 dp[3]
+
+        for(int i = 4 ; i <= t ; i++){
+            dp[i] = max(dp[i-1],max(dp[i-2] + vc[i], dp[i-3] + vc[i] + vc[i-1]));   // 3가지 시나리오를 비교
+            result = max(result, dp[i]);                                            // 결과 저장
+        }
+
+        cout << result; // 결과 출력
+        return 0;
     }
-
-    dp[1] = al[0];
-    dp[2] = al[0] + al[1];
-
-    if(t == 1) {cout << dp[1]; return 0;}
-    if(t == 2) {cout << dp[2]; return 0;}
-
-    for(int i = 3 ; i <= t ; i++){
-        int case1 = dp[i-2] + al[i-1];
-        int case2 = dp[i-3] + al[i-1] + al[i-2];
-        int case3 = dp[i-1];
-
-        dp[i] = max(case3,max(case1,case2));
-    }
-
-    // 디버깅
-    // for(int i = 0 ; i <= t ; i++){
-    //     cout << i << ": " << dp[i] << "\n";
-    // }
-
-    cout << dp[t];
-}
