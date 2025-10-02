a1a98da68c34f7f45be5310c9251f8b055eab875
okonys <62321546+ysk1007@users.noreply.github.com>
2025-10-02 15:17:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,45 +1,39 @@
 #include<iostream>
+
 using namespace std;
 
 // 쉬운 계단 수 #10844
+
 int main(){
     int n;
-    long long dp[101][10] = {0};
-    cin >> n;
+    long long dp[101][10] = {0};            // dp[자리수][0~10으로 끝나는 수]
 
-    // 한 자리 계단 수는 모두 1임
-    for(int i = 1 ; i <= 9 ; i++){
+    cin >> n;
+    for(int i = 0 ; i <= 9 ; i++){
+        if(i==0) continue;
         dp[1][i] = 1;
     }
 
-    // 두 자리 이상 계단 수 구하기
-    for(int i = 2 ; i <= n ; i++){
-        for(int j = 0 ; j <= 9 ; j++){
-            if(j == 0) { dp[i][j] += dp[i-1][1]; }          // 이전 수가 0이 오려면 경우의 수는 1밖에 없음
-            else if(j == 9){ dp[i][j] += dp[i-1][8]; }      // 이전 수가 9가 오려면 경우의 수는 8밖에 없음
-            else { 
-                dp[i][j] += dp[i-1][j-1] + dp[i-1][j+1];    // 이전 수가 j(그 외)가 오려면 경우의 수는 2가지씩 있음
-            }                
+    for(int j = 2 ; j <= n ; j++){
+        for(int k = 0 ; k <= 9 ; k++){
+            // 0 로 끝나는 수는 1로 끝나야만 만들 수 있다.
+            if(k==0) dp[j][0] = dp[j-1][1] % 1000000000LL;
 
-            dp[i][j] %= 1000000000LL;
-        }
+            // 9 로 끝나는 수는 8로 끝나야만 만들 수 있다.
+            else if(k==9) dp[j][9] = dp[j-1][8] % 1000000000LL;
+
+            // 1 ~ 8 까지 각 끝나는 수는 n + 1, n - 1 가 만들어 준다.
+            else{
+                dp[j][k] = dp[j-1][k-1] + dp[j-1][k+1] % 1000000000LL;
+            }
+        }        
     }
 
-    // 마지막 단 합계
+    // 결과 출력
     long long sum = 0;
-    for(int i = 0 ; i <= 9 ; i++){
-        sum = (sum + dp[n][i]) % 1000000000LL;
+    for(int l = 0 ; l <= 9 ; l++){
+        sum += dp[n][l] % 1000000000LL;
     }
 
-    // 디버깅
-    // for(int i = 0 ; i <= n ; i++){
-    //     for(int j = 0 ; j <= 9 ; j++){
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
-
-    // 출력
-    cout << sum;
-    return 0;
+    cout << sum % 1000000000LL;;
 }
