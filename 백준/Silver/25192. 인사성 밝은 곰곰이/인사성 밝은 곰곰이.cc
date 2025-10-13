89eee17703c2e3c0395476df0d8940310c744473
okonys <62321546+ysk1007@users.noreply.github.com>
2025-10-13 17:18:58 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 88 ms, Memory: 8892 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -4,30 +4,33 @@ using namespace std;
 
 // 인사성 밝은 곰곰이 #25192
 int main(){
+
+    // 빠른 입출력
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t, count = 0;
-    string str;
-    map<string,bool> mp;
-    bool flag = false;
-    cin >> t;
+    int t, result = 0;
+    string name;
+    map<string,bool> check;
 
-    for(int i = 0 ; i < t ; i++){
-        cin >> str;
-        if(str == "ENTER" && !flag){
-            flag = true;
+    cin >> t;                   // 데이터 수
+    
+    while(t--){
+        cin >> name;            // 이름 입력 받음
+
+        if(name == "ENTER"){    // 새로운 사용자가 입장
+            check.clear();      // 첫인사 여부 체크 맵을 초기화
+            continue;
         }
-        else if(str == "ENTER" && flag){
-            mp = map<string,bool>();
-        }
-        else{
-            if(mp[str]) continue;
-            count++;
-            mp[str] = true;
+
+        if(!check[name]){       // 이름이 들어왔는데 첫인사를 안 했음
+            check[name] = true; // 첫인사 체크하고
+            result++;           // 카운트 +1
         }
     }
 
-    cout << count;
+    cout << result;             // 결과 출력
+
+    return 0;
 }
